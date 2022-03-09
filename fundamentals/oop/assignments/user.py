3a4dac9f9c60ae4e721ac382ca6551353f76811d
Mustafa Wahid <mustafarwahid@gmail.com>
2022-03-09 15:37:32 -0500

    - added transfer function

--- fundamentals/oop/assignments/user.py
+++ fundamentals/oop/assignments/user.py
@@ -14,6 +14,11 @@ class User:
     def display_user_name(self): #function display user name
         print(self.name)
         return self
+    def transfer_money(self, other_user, amount): 
+        self.balance -= amount
+        other_user.balance += amount
+        print(f"{self.name} makes a transfer of {amount} to {other_user.name}")
+        return self
 
 # #user 1
 x = User("Mindy") #create user
@@ -27,3 +32,7 @@ y.display_user_name().make_desposit(500).make_desposit(500).make_withdrawal(1000
 
 #user 3 1 d, 3w
 z = User("Jeff").display_user_name().make_desposit(500000).make_withdrawal(1000).make_withdrawal(1000).make_withdrawal(1000).display_user_balance()
+z.transfer_money(y,50000)
+
+
+
