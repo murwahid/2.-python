f7831edb2842a0f38ffcce943fef2f735c8b6674
Mustafa Wahid <mustafarwahid@gmail.com>
2022-03-13 14:13:06 -0400

    - initial commit

---
+++ flask/assignments/playground/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello"
+
+#PLAY FUNCTION
+@app.route('/play')
+def display():
+    return render_template('blueBoxes.html')
+
+#PLAY W/ NUMBERS FUNCTION
+@app.route('/play/<int:num>/<string:boxColor>')
+def display_boxes(num,boxColor):
+    times = int(num)
+    return render_template('blueBoxes.html', num=num,boxColor=boxColor)
+
+#! MUST BE AT THE BOTTOM ---------------
+if __name__ == "__main__":
+    app.run(debug=True)
